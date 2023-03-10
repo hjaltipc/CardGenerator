a83a7a50511ffe06841dfe96b491b094496a95de
Hjalti <43811475+hjaltipc@users.noreply.github.com>
2023-03-10 09:50:16 +0100

    Card back function completed

--- Generator.py
+++ Generator.py
@@ -2,19 +2,19 @@ from fpdf import FPDF
 import json
 
 
-def generateCardBacks():
+def generateCardBacks(CardType,skipping):
     row = 0
     column = 4
     #Adds new page for back of sector cards
     pdf.add_page()
-    for (index,item) in enumerate(data["Sectors"][:-2]):
+    for (index,item) in enumerate(data[CardType][:-skipping]):
         column -= 1
         if column == -1:
             row += 1
             column = 3
         pdf.set_y((col_body+col_title)*row)
         pdf.set_x(50 * column)
-        pdf.cell(col_width, col_title+col_body, txt = "Sectors", border = 1, align = 'C')
+        pdf.cell(col_width, col_title+col_body, txt = CardType, border = 1, align = 'C')
 
 #opens and returns file content as dictionary
 f = open('StartupSuperfight.json')
@@ -31,6 +31,7 @@ pdf.set_font("Arial", size = 15)
 col_width = 50
 col_title = 20
 col_body = 50
+
 row = 0
 column = 0
 
@@ -45,7 +46,22 @@ for (index,item) in enumerate(data["Sectors"][:-2]):
     pdf.cell(col_width, col_title, txt = item, border = "LRT", ln = 2, align = 'C')
     pdf.cell(col_width, col_body, txt = "Shrek", border = "LRB", align = 'C')
     column += 1
-generateCardBacks()
+generateCardBacks("Sectors", 2)
+
+pdf.add_page()
+row = 0
+column = 0
+for (index,item) in enumerate(data["Weaknesses"][:-2]):
+    if (column+1)%5 == 0:
+        row += 1
+        column = 0
+    print(index, item)
+    pdf.set_y((col_body+col_title)*row)
+    pdf.set_x(50 * column)
+    pdf.cell(col_width, col_title, txt = item, border = "LRT", ln = 2, align = 'C')
+    pdf.cell(col_width, col_body, txt = "Shrek", border = "LRB", align = 'C')
+    column += 1
+generateCardBacks("Weaknesses",2)
 
 
 #Adds new page for App cards
